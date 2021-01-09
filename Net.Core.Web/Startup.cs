bab3ab6a95671b51e417f945af43935d38a4c527
Federico Barraza <fede.barraza@outlook.com>
2021-01-09 15:10:43 -0300

    Error Handling and configuration

--- Net.Core.Web/Startup.cs
+++ Net.Core.Web/Startup.cs
@@ -1,17 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Net.Core.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,20 +24,24 @@ namespace Net.Core.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            if (_configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
+                if (context.Request.Path.Value.Contains("invalid"))
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    throw new Exception("ERROR!");
+                }
+
+                await next();
             });
+
+            app.UseFileServer();
         }
     }
 }
